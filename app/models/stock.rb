74ce265c1b648e5d1dc5849bfc5f89ab3f5fd496
Aditi Darji <adudarji2002@gmail.com>
2023-08-24 13:51:38 +0530

    Add Stock model

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -2,7 +2,7 @@ class Stock < ApplicationRecord
     def self.new_lookup(ticker_symbol)
         client = IEX::Api::Client.new(
             publishable_token: Rails.application.credentials.iex_client[:access_key_id],
-            secret_token: 'sk_becf5ff3d77b462d83238ca503aec081',
+            secret_token: Rails.application.credentials.iex_client[:secret_access_key],
             endpoint: 'https://cloud.iexapis.com/v1'
           )
         client.price(ticker_symbol)
